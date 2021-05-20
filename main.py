d9293dfad1fca8275d943f49cf9efcfe9f5ee88c
BhagatAastha <>
2021-05-20 23:22:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# Import the system library and pygame library 
+import sys, pygame
+# Initialize pygame 
+pygame.init()
+
+# Setting Window Size
+size = width, height = 650, 350
+# Creating a display window with size
+screen = pygame.display.set_mode(size)
+
+# Loading a ball image
+ball = pygame.image.load("intro_ball.gif")
+# Measure the ball size in rectangle 
+ballrect = ball.get_rect()
+
+# Setting the Ball speed 
+speed = [1,1]
+# Setting color (black; 0,0,0)
+black =0,0,0
+while True:
+  # When the 'X', exit button pressed, game terminates
+  for event in pygame.event.get():
+    if(event.type == pygame.QUIT):
+      sys.exit()
+
+  # Changing ball direction after bouncing
+  ballrect = ballrect.move(speed)
+
+  if(ballrect.left < 0 or ballrect.right> width):
+    speed[0] = -speed[0]
+  if(ballrect.top < 0 or ballrect.bottom > height):
+    speed[1] = -speed[1]
+
+  # Setting background color(solid black or random color)
+  screen.fill(black)
+  # Updating the screen and the ball position
+  screen.blit(ball,ballrect)
+  pygame.display.flip()
